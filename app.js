26542029dc89f2e1d607a921354676a24586458b
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2018-10-22 21:31:23 +0100

    finish lab steps (RESTful API)

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const donations = require("./routes/donations");
+
 var app = express();
 
 // view engine setup
@@ -22,20 +24,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// Our Custom Donation Web App Routes
+app.get('/donations', donations.findAll);
+app.get('/donations/votes', donations.findTotalVotes);
+app.get('/donations/:id', donations.findOne);
+
+app.post('/donations',donations.addDonation);
+
+app.put('/donations/:id/vote', donations.incrementUpvotes);
+
+app.delete('/donations/:id', donations.deleteDonation);
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
+
+
+
 
 module.exports = app;
